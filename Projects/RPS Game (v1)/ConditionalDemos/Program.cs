d30d11094cdb1ff1b83edc277e4156b978eb5d22
Michael Hadley <michaelwesthadley@gmail.com>
2021-02-15 16:22:43 -0600

    Add RPS game from week 03

---
+++ Projects/RPS Game (v1)/ConditionalDemos/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using static System.Console;
+
+namespace ConditionalDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "~~~ Conditional Demos ~~~";
+
+            // Trivia Conditional Example:
+            //WriteLine("What is the national animal of Scotland: unicorn or dragon?");
+            //string playerResponse = ReadLine();
+            //if (playerResponse == "unicorn")
+            //{
+            //    WriteLine("Correct!");
+            //    WriteLine("It originates from Celtic mythology.");
+            //}
+            //else
+            //{
+            //    WriteLine("Wrong!");
+            //    WriteLine("But that would be cool though...");
+            //}
+
+            // Temp Clothing Conditional Example:
+            //WriteLine("What is the current temp in F?");
+            //string tempResponse = ReadLine();
+            //int temp = Convert.ToInt32(tempResponse);
+            ////WriteLine($"Is temp 0? {temp == 0}"); // Equality
+            ////WriteLine($"Is temp anything but 0? {temp != 0}"); // Inequality
+            ////WriteLine($"Is temp greater than 0? {temp > 0}");
+            ////WriteLine($"Is temp greater than or equal to 0? {temp >= 0}");
+            ////WriteLine($"Is temp less than 0? {temp < 0}");
+            ////WriteLine($"Is temp less than or equal to 0? {temp <= 0}");
+            //if (temp <= 0)
+            //{
+            //    WriteLine("Don't go outside - frostbite warning!");
+            //}
+            //else if (temp <= 32)
+            //{
+            //    // 1 - 32 degrees F
+            //    WriteLine("Bring a heavy coat if you go outside.");
+            //}
+            //else if(temp <= 65)
+            //{
+            //    // 33 - 65 degrees F
+            //    WriteLine("Wear a sweater if you go outside.");
+            //}
+            //else if(temp <= 100)
+            //{
+            //    // 66 - 100 degrees F
+            //    WriteLine("Bring a hat, sunscreen and water.");
+            //}
+            //else
+            //{
+            //    // 101+ degrees F
+            //    WriteLine("Don't go outside without a personal AC.");
+            //}
+
+            // RPS Game Starts
+            RPSGame myRPSGame = new RPSGame();
+            myRPSGame.Play();
+            
+            WriteLine("Press any key to exit...");
+            ReadKey();
+        }
+    }
+}
---
+++ Projects/RPS Game (v1)/ConditionalDemos/RPSGame.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace ConditionalDemos
+{
+    class RPSGame
+    {
+        private string RockGraphic = @"    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)";
+        private string ScissorsGraphic = @"    _______
+---'   ____)____
+          ______)
+        __________)
+      (____)
+---.__(___)";
+        private string PaperGraphic = @"    _______
+---'   ____)____
+          ______)
+          _______)
+          _______)
+---.__________)";
+        private string ErrorGraphic = @"  ___ _ __ _ __ ___  _ __ 
+/ _ \ '__| '__/ _ \| '__|
+|  __/ |  | | | (_) | |   
+\___|_|  |_|  \___/|_|";
+
+        public void Play()
+        {
+            WriteLine("~~~ RPS Game ~~~");
+
+            WriteLine("Would you like to play? (yes/no)");
+            string playResponse = ReadLine().Trim().ToLower();
+            //string playResponseLower = playResponse.ToLower();
+            //string playResponseTrimmed = playResponseLower.Trim();
+
+
+            if (playResponse == "yes")
+            {
+                WriteLine("Great. Let's play.");
+                WriteLine("Rock, paper or scissors?");
+                string playerMove = ReadLine().Trim().ToLower();
+
+                WriteLine("");
+                if (playerMove == "rock")
+                {
+                    ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("You played rock...");
+                    WriteLine(RockGraphic);
+                    WriteLine("");
+
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("The AI played paper!");
+                    WriteLine(PaperGraphic);
+                    
+                    WriteLine("\nYou lose.");
+                    ForegroundColor = ConsoleColor.White;
+                }
+                else if (playerMove == "paper")
+                {
+                    ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("You played paper...");
+                    WriteLine(PaperGraphic);
+                    WriteLine("");
+
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("The AI played scissors!");
+                    WriteLine(ScissorsGraphic);
+
+                    WriteLine("\nYou lose.");
+                    ForegroundColor = ConsoleColor.White;
+                }
+                else if (playerMove == "scissors")
+                {
+                    ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("You played scissors...");
+                    WriteLine(ScissorsGraphic);
+                    WriteLine("");
+
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("The AI played rock!");
+                    WriteLine(RockGraphic);
+
+                    WriteLine("\nYou lose.");
+                    ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine($"You played: {playerMove}...");
+                    WriteLine(ErrorGraphic);
+                    WriteLine("Error, error. The AI broke.");
+                    ForegroundColor = ConsoleColor.White;
+                    WriteLine("\nYou win!");
+                }
+
+                WriteLine("\nWould you like to play again? (yes/no)");
+                string playAgainResponse = ReadLine().ToLower().Trim();
+                if (playAgainResponse == "yes")
+                {
+                    Clear();
+
+                    // Recursion!
+                    Play();
+                }
+                else
+                {
+                    WriteLine("It was fun playing, see you again soon.");
+                }
+            }
+            else
+            {
+                WriteLine("Aw shucks, maybe next time.");
+            }
+        }
+    }
+}
